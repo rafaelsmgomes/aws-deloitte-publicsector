bddf0ca03a349ad158918776e4f478180456d074
Rafael Gomes <rafael.smgomes@gmail.com>
2020-07-29 19:42:10 +0200

    New anim + func on top of a lot of html changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,16 @@
 import '../sass/main.scss';
 
+import $ from 'jquery';
+
 import { gsap } from "gsap";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { setTimeout } from 'core-js';
 
 gsap.registerPlugin(ScrollToPlugin, ScrollTrigger, DrawSVGPlugin);
+
+
 
 
 function gsapScroll(id) {
@@ -30,71 +35,28 @@ indicator3.addEventListener('click', () => gsapScroll('#quote-1'))
 indicator4.addEventListener('click', () => gsapScroll('#better-view'))
 indicator5.addEventListener('click', () => gsapScroll('#solutions'));
 
-gsap.to(
-  indicator1, {
-  scrollTrigger: {
-    trigger: '#header',
-    start: "top center",
-    end: "bottom top",
-    toggleActions: 'restart reverse restart reverse',
-    markers: true
-  },
-  backgroundColor: '#c5a1ff',
-  ease: 'none',
-  duration: .25
-})
-gsap.to(
-  indicator2, {
-  scrollTrigger: {
-    trigger: '#industry',
-    start: "top center",
-    end: "bottom center",
-    toggleActions: 'restart reverse restart reverse',
-    markers: true
-  },
-  backgroundColor: '#c5a1ff',
-  ease: 'none',
-  duration: .25
-})
-gsap.to(
-  indicator3, {
-  scrollTrigger: {
-    trigger: '#quote-1',
-    start: "top center",
-    end: "bottom center",
-    toggleActions: 'restart reverse restart reverse',
-    markers: true
-  },
-  backgroundColor: '#c5a1ff',
-  ease: 'none',
-  duration: .25
-})
-gsap.to(
-  indicator4, {
-  scrollTrigger: {
-    trigger: '#better-view',
-    start: "top center",
-    end: "bottom center",
-    toggleActions: 'restart reverse restart reverse',
-    markers: true
-  },
-  backgroundColor: '#c5a1ff',
-  ease: 'none',
-  duration: .25
-})
-gsap.to(
-  indicator5, {
-  scrollTrigger: {
-    trigger: '#solutions',
-    start: "top center",
-    end: "bottom center",
-    toggleActions: 'restart reverse restart reverse',
-    markers: true
-  },
-  backgroundColor: '#c5a1ff',
-  ease: 'none',
-  duration: .25
-})
+gsapColorSection(indicator1, '#header')
+gsapColorSection(indicator2, '#industry')
+gsapColorSection(indicator3, '#quote-1')
+gsapColorSection(indicator4, '#better-view')
+gsapColorSection(indicator5, '#solutions')
+
+function gsapColorSection(indicator, sectionId) {
+  gsap.to(
+    indicator, {
+    scrollTrigger: {
+      trigger: sectionId,
+      start: "top center",
+      end: "bottom center",
+      toggleActions: 'restart reverse restart reverse',
+      // markers: true
+    },
+    backgroundColor: '#c5a1ff',
+    ease: 'none',
+    duration: .25
+  })
+}
+
 
 
 
@@ -110,6 +72,14 @@ gsap.to('.industry__img', {
   duration: .5,
   ease: 'Power1.easeIn'
 })
+
+// scrollTrigger adds class active 
+// element.addEventListener('hover', () => {
+//   gsap.to('.active', {
+//     duration: 1,
+//     height: 'auto'
+//   })
+// })
 
 gsap.from('.focus__img *', {
   duration: 2,
@@ -137,12 +107,22 @@ gsap.from('#prefix__Path_1253', {
 })
 // scrollTrigger.create()
 
+
+/*
+Sticky Navigation Function
+*/
+
+
 window.onscroll = () => {
-  addStickyNav()
+  addStickyNav();
 }
 
 const navbar = document.querySelector('.navigation')
-const sticky = navbar.offsetTop;
+const header = document.querySelector('#header')
+const sticky = header.offsetHeight;
+console.log(navbar.offsetTop + header.offsetHeight)
+console.log(navbar.offsetTop)
+console.log(header.offsetHeight)
 function addStickyNav() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add('sticky')
@@ -150,3 +130,107 @@ function addStickyNav() {
     navbar.classList.remove('sticky')
   }
 }
+
+const subList2 = $('.nav__sub-list-2')
+const subList3 = $('.nav__sub-list-3')
+const subItem2 = $('.nav__item-2')
+const subItem3 = $('.nav__item-3')
+
+
+const hideSub = (el) => {
+  // If list doesn't contain class closed - .close it 
+  if (!el.classList.contains('closed')) {
+    gsap.to(el, {
+      duration: .25,
+      autoAlpha: 0,
+      height: 0
+    });
+    el.classList.add('closed')
+  } else { // else open it AND remove .close
+    gsap.set(el, {
+      height: 'auto',
+      autoAlpha: 1
+    })
+    gsap.from(el, {
+      duration: .25,
+      autoAlpha: 0,
+      height: 0
+    });
+
+    el.classList.remove('closed')
+  }
+}
+
+var setTimeoutFunction;
+$('.nav__open-1').hover(function () {
+  setTimeoutFunction = setTimeout(() => {
+    $('.nav__open-1').addClass('active');
+    gsap.to('.active > .nav__sub-list-1', {
+      height: 'auto',
+      autoAlpha: 1,
+      duration: .25
+    })
+  }, 250);
+}, () => {
+  $('.nav__open-1').removeClass('active')
+  gsap.to('.nav__sub-list-1', {
+    height: 0,
+    autoAlpha: 0,
+    duration: .25
+  })
+  clearTimeout(setTimeoutFunction)
+})
+$('.nav__open-2').hover(function () {
+  setTimeoutFunction = setTimeout(() => {
+    $('.nav__open-2').addClass('active');
+    gsap.to('.active > .nav__sub-list-2', {
+      height: 'auto',
+      autoAlpha: 1,
+      duration: .25
+    })
+  }, 250);
+}, () => {
+  $('.nav__open-2').removeClass('active')
+  gsap.to('.nav__sub-list-2', {
+    height: 0,
+    autoAlpha: 0,
+    duration: .25
+  })
+  clearTimeout(setTimeoutFunction)
+})
+
+
+
+
+
+// ----------------------------------------------------------------
+// Not a great solution to the Navigation animation problem
+// ----------------------------------------------------------------
+const navMenu = $('.nav__menu')
+$('.navigation').hover(() => {
+  if ($('.navigation').hasClass('sticky')) {
+    gsap.to('.nav__menu', {
+      height: 'auto',
+      maxHeight: 'auto',
+      duration: .3,
+      autoAlpha: 1
+    })
+  }
+},
+  function () {
+    if ($('.navigation').hasClass('sticky')) {
+      gsap.to(navMenu, {
+        height: 0,
+        duration: .3,
+        autoAlpha: 0,
+        onComplete: () => {
+          gsap.to('.nav__menu', { clearProps: true })
+        }
+
+      })
+    }
+  }
+)
+
+
+
